ae5ded2cf6fe19a26aea3741fb74ae9bc4d5505e
Shivangi Gagneja <98452924+shivi1224@users.noreply.github.com>
2024-02-06 14:07:44 +0530

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,33 @@
+// Java Program to Add N Numbers
+import java.io.*;
+import java.util.*;
+
+// Driver Class
+class Program {
+	// main function
+	public static void main(String[] args)
+	{
+
+		// N is the number if elements
+		int N;
+
+		// Initialising the Scanner Class
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter the Number of elements:"
+
+		// Taking the input of N
+		N = sc.nextInt();
+		int sum = 0;
+
+		// Taking N inputs and find the sum
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+
+			sum += a;
+		}
+
+		// Printing the sum of N numbers
+		System.out.println("Sum of Input Numbers : " + sum);
+	}
+}
